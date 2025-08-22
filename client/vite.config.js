0d2efd6d717458ad5f799e92acdabe4d25187f60
Ankuxkashyap <ankitdochak0001@gmail.com>
2025-08-23 00:40:58 +0530

    fix some deployment issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://mern-job-portal-0ey2.onrender.com/',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
